c3440972e7206f0e6662bfa2a96fb800543379ce
Rizqi <rizqiaditya222@gmail.com>
2025-12-25 00:01:34 +0700

    fix(docs): fixing documentations

--- src/docs/swagger.js
+++ src/docs/swagger.js
@@ -3,15 +3,18 @@ const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDefinition = require('../../swaggerDef');
 
+
 const options = {
   swaggerDefinition,
-  apis: ['./src/routes/*.js', './src/controllers/*.js'],
+  apis: ['src/routes/*.js', 'src/controllers/*.js'], // path relatif dari root project
 };
 
 const swaggerSpec = swaggerJSDoc(options);
 
+
 function setupSwagger(app) {
   app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.get('/swagger.json', (req, res) => res.json(swaggerSpec));
 }
 
 module.exports = setupSwagger;
