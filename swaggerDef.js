dcaddfff5c4bbba18d4093176443b97b763e002e
Rizqi <rizqiaditya222@gmail.com>
2025-12-24 23:45:29 +0700

    fix(docs): adding new url

--- swaggerDef.js
+++ swaggerDef.js
@@ -1,4 +1,3 @@
-// swaggerDef.js
 module.exports = {
   openapi: '3.0.0',
   info: {
@@ -10,6 +9,10 @@ module.exports = {
     {
       url: 'http://localhost:3000',
       description: 'Local server',
+    },
+    {
+      url: 'http://159.223.67.39:3000',
+      description: 'VPS server',
     },
   ],
 };
